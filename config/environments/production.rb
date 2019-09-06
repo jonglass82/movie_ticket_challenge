f36cc6abec13fa321b286feb289a93c33204e733
Jon Glass <jonglass82@gmail.com>
2019-09-06 12:57:22 -0500

    add mailer config to config/production.rb for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,20 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'glassmantheaters@gmail.com'}
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address:              'smtp.gmail.com',
+port:                 587,
+domain:               'https://safe-chamber-94135.herokuapp.com',
+user_name:            "#{ENV["THEATER_EMAIL"]}",
+password:             "#{ENV["THEATER_PASSWORD"]}",
+authentication:       'plain',
+enable_starttls_auto: true  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
